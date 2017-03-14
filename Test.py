620ef5461aa2afe989b0f59113a9034ab3887c73
Totrock <josef.probst7@googlemail.com>
2017-03-14 15:50:12 +0100

    test1

--- Test.py
+++ Test.py
@@ -1,11 +1,2 @@
 import json
 
-json_string = '{"first_name": "Guido", "last_name":"Rossum"}'
-parsed_json = json.loads(json_string)
-print(parsed_json['first_name'])
-
-
-with open('data.txt', 'w') as outfile:
-    json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
-
-print (json.dumps("\"foo\bar"))
